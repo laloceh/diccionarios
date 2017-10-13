4e08964debb6a5221a8e6a485fbc3e499ee0483f
eduardo <eduardo@10.0.2.15>
2017-10-13 10:27:31 -0600

    Implementado el HashMap

--- Diccionarios/src/diccionarios/Diccionarios.java
+++ Diccionarios/src/diccionarios/Diccionarios.java
@@ -5,6 +5,10 @@
  */
 package diccionarios;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  *
  * @author eduardo
@@ -16,6 +20,32 @@ public class Diccionarios {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        //Uso de HashMap
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "Casillas");
+        map.put(3, "Pique");
+        map.put(11, "Capdevila");
+        map.put(16, "Busquets");
+        map.put(18, "Pedrito");
+        map.put(7, "Vila");
+        map.put(15, "Ramos");
+        map.put(5, "Puyol");
+        map.put(5, "Xabi Alonso");
+        map.put(8, "Xavi Hernandez");
+        map.put(6, "Iniesta");
+        
+        //Imprimimos el Map con un iterador
+        Iterator it = map.keySet().iterator();
+        while(it.hasNext())
+        {
+            Integer key = (int)it.next();
+            System.out.println("Clave: " + key + " -> Valor: " + map.get(key));
+            
+        }
+        
+        
+        
+    }  //main
     
 }
