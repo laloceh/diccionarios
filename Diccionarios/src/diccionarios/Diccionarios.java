2b7fc4cf366b855e27f25087fc877412cdc433eb
eduardo <eduardo@10.0.2.15>
2017-10-13 11:38:48 -0600

    Implementa TreeMap de jugadores de seleccion, e imprime con el metodo ImprimeAsArrayJugadorSeleccion

--- Diccionarios/src/diccionarios/Diccionarios.java
+++ Diccionarios/src/diccionarios/Diccionarios.java
@@ -106,6 +106,27 @@ public class Diccionarios {
         System.out.println("");
         Funciones.ImprimeAsArray(linkedHashMap);
         
+        //Probamos usando un objeto en el map
+        //vamos a crear un TreeMap en la que la clave será el nombre del jugador 
+        //y el valor será un objeto de la clase "JugadorSeleccion".
+        System.out.println("");
+        System.out.println("******** TreeMap con Objetos y como Clave un Sting ********");
+        Map<String, JugadorSeleccion> jugadores = new TreeMap<>();
+        jugadores.put("Casillas", new JugadorSeleccion(1, "Casillas", "Portero"));
+        jugadores.put("Ramos", new JugadorSeleccion(15, "Ramos", "Lateral Derecho"));
+        jugadores.put("Pique", new JugadorSeleccion(13, "Pique", "Central"));
+        jugadores.put("Puyol", new JugadorSeleccion(5, "Puyol", "Central"));
+        jugadores.put("Capdevila", new JugadorSeleccion(11, "Capdevila", "Lateral Izquierdo"));
+        jugadores.put("Xabi", new JugadorSeleccion(14, "Xabi Alonso", "Medio Centro"));
+        jugadores.put("Busquets", new JugadorSeleccion(16, "Busquets", "Medio Centro"));
+        jugadores.put("Xavi", new JugadorSeleccion(8, "Xavi", "Centro Campista"));
+        jugadores.put("Pedrito", new JugadorSeleccion(18, "Pedrito", "Interior Izquierdo"));
+        jugadores.put("Iniesta", new JugadorSeleccion(6, "Iniesta", "Interior Derecho"));
+        jugadores.put("Villa", new JugadorSeleccion(7, "Villa", "Delantero"));
+        
+        //Imprimimos
+        Funciones.ImprimeAsArrayJugadorSeleccion(jugadores);
+        
     }  //main
     
 }
