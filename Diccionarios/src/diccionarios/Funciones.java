54944f58a86e2d691799a9581d65f889c7abb0ec
eduardo <eduardo@10.0.2.15>
2017-10-13 10:58:04 -0600

    ImprimeMetodos

--- Diccionarios/src/diccionarios/Funciones.java
+++ Diccionarios/src/diccionarios/Funciones.java
@@ -5,10 +5,31 @@
  */
 package diccionarios;
 
+import java.util.Map;
+
 /**
  *
  * @author eduardo
  */
 public class Funciones {
+   
+    public static void ImprimeMetodos(Map<Integer,String> treeMap)
+    {
+        System.out.println("******** Trabajando con los metodos de Map ********"); 
+        System.out.println("Mostramos el numero de elementos que tiene el TreeMap: treeMap.size() = " + treeMap.size());
+        System.out.println("Vemos si el TreeMapesta vacio: treeMap.isEmpty() = " + treeMap.isEmpty());
+        System.out.println("Obtenemos un elemento del Map pasandole la clave 6: treeMap.get(6) = " + treeMap.get(6));
+        System.out.println("Borramos un elemento del Map, el 18 : treeMap.remove(18) = " + treeMap.remove(18));
+        System.out.println("Vemos que pasa si queremos obtener la clave 18 que ya no existe: treeMap.get(18) = " + treeMap.get(18));
+        System.out.println("Vemos si existe un elemento con la clave 18: treeMap.containsKey(18) = " + treeMap.containsKey(18));
+        System.out.println("Vemos si existe un elemento con la clave 1: treeMap.containsKey(1) = " + treeMap.containsKey(1));
+        System.out.println("Vemos si existe el valor 'Villa' en el Map: treeMap.containsValue('Villa') = " + treeMap.containsValue("Villa"));
+        System.out.println("Vemos si existe el valor 'Ricardo' en el Map: treeMap.containsValue('Ricardo') = " + treeMap.containsValue("Ricardo"));
+        System.out.println("Borramos todos los elementos del Map: treeMap.clear()"); treeMap.clear();
+        System.out.println("Comprobamos si hemos eliminado viendo su tamanio: treeMap.size() = " + treeMap.size());
+        System.out.println("Lo comprobamos tambien viendo si esta vacio: treeMap.isEmpty() = " + treeMap.isEmpty());
+        
+    }
     
-}
+} //END
+
